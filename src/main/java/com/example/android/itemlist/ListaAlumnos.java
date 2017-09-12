6817c9a3bb0259aea744be5a45abef2fcf882f8b
JavierNegrete <janedaz@hotmail.com>
2017-09-11 22:23:10 -0500

    Add files via upload

---
+++ src/main/java/com/example/android/itemlist/ListaAlumnos.java
@@ -0,0 +1,14 @@
+package com.example.android.itemlist;
+
+public class ListaAlumnos {
+
+    public static String[] getAlumnos(){
+        String[] alumnos = {"Andres","Martin","Angel","Santiago",
+                "Ernesto","Miguel","Ricardo","Laura","José",
+                "Pedro","Javier","Joel","Fernando","Hugo",
+                "Tauro","Gustavo","Hilario","Rosendo"
+        };
+        return alumnos;
+    }
+
+}
---
+++ src/main/java/com/example/android/itemlist/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.itemlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView itemList;
+
+        itemList = (TextView)findViewById(R.id.textViewItemLst);
+
+        String[] listaAlumno = ListaAlumnos.getAlumnos();
+
+        for ( String alumno : listaAlumno  ) {
+            itemList.append(alumno + "\n\n\n");
+        }
+    }
+
+
+}
